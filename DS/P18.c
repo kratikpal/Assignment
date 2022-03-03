2475f2ad29214a5e0cfbc5e59e1c7d85b20e7814
Kratikpal <kratikpal@gmail.com>
2022-03-03 17:12:37 +0530

    sorting

---
+++ DS/P18.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <conio.h>
+#define size 5
+
+int partition(int arr[], int start, int end)
+{
+    int pivot = arr[end];
+    int i = (start - 1);
+
+    for (int j = start; j <= end - 1; j++)
+    {
+
+        if (arr[j] < pivot)
+        {
+            i++;
+            int t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+        }
+    }
+    int t = arr[i + 1];
+    arr[i + 1] = arr[end];
+    arr[end] = t;
+    return (i + 1);
+}
+
+void quickSort(int arr[], int start, int end)
+{
+    if (start < end)
+    {
+        int p = partition(arr, start, end);
+        quickSort(arr, start, p - 1);
+        quickSort(arr, p + 1, end);
+    }
+}
+
+int main()
+{
+    int arr[size], i;
+    printf("Enter 5 element of array\n");
+    for (i = 0; i < size; i++)
+        scanf("%d", &arr[i]);
+    quickSort(arr, 0, size - 1);
+    printf("\nAfter sorting array elements are - \n");
+    for (i = 0; i < size; i++)
+        printf("%d\t", arr[i]);
+    getch();
+
+    return 0;
+}
---
+++ DS/P19.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#define size 5
+
+void merge(int arr[], int mid, int low, int high)
+{
+    int i, j, k, B[100];
+    i = low;
+    j = mid + 1;
+    k = low;
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] < arr[j])
+        {
+            B[k] = arr[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            B[k] = arr[j];
+            j++;
+            k++;
+        }
+    }
+    while (i <= mid)
+    {
+        B[k] = arr[i];
+        k++;
+        i++;
+    }
+    while (j <= high)
+    {
+        B[k] = arr[j];
+        k++;
+        j++;
+    }
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = B[i];
+    }
+}
+
+void mergeSort(int arr[], int low, int high)
+{
+    int mid;
+    if (low < high)
+    {
+        mid = (low + high) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, mid, low, high);
+    }
+}
+
+int main()
+{
+    int arr[size], i;
+    printf("Enter 5 element of array\n");
+    for (i = 0; i < size; i++)
+        scanf("%d", &arr[i]);
+    mergeSort(arr, 0, size - 1);
+    printf("\nAfter sorting array elements are - \n");
+    for (i = 0; i < size; i++)
+        printf("%d\t", arr[i]);
+    return 0;
+}
