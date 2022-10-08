77ada493b603d600dbf0110836065836ccf7daa7
Kratikpal <kratikpal@gmail.com>
2022-10-08 13:41:53 +0530

    A6-P4 of R

---
+++ R/A1/5.r
@@ -0,0 +1,15 @@
+#Write a R program to demonstrate operator
+
+a <- 6
+b <- 2
+l1 <- T
+l2 <- F
+print(paste("+ operator = ",a+b))
+print(paste("- operator = ",a-b))
+print(paste("* operator = ",a*b))
+print(paste("/ operator = ",a/b))
+print(paste("logical AND = ",l1&&l2))
+print(paste("logical Or = ",l1||l2))
+print(paste("Negation  = ",!l1))
+print(paste("less than  = ",a<b))
+print(paste("greater than  = ",a>b))
---
+++ R/A1/6.r
@@ -0,0 +1,7 @@
+#Write a R program to assign the values to the variable
+a <- 1
+b = 2
+3 -> c
+print(a)
+print(b)
+print(c)
