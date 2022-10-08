aea2cacc781fd16b0ae835a4aad10e9da8005c96
Kratikpal <kratikpal@gmail.com>
2022-10-08 13:23:26 +0530

    A1-P4 of R

---
+++ R/A1/4.r
@@ -0,0 +1,12 @@
+#Write a R program to demonstrate data types
+
+char1 <- "Character"
+num1 <- 2.3
+int1 <- 2L
+logi1 <- T
+comp1 <- 2+4i
+print(class(char1))
+print(class(num1))
+print(class(int1))
+print(class(logi1))
+print(class(comp1))
