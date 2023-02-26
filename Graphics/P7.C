432a580d876aa889b7fb7264fde27a22297ad7f4
Kratikpal <kratikpal@gmail.com>
2023-02-26 07:18:18 -0800

    P7 of Graphics

--- Graphics/P7.C
+++ Graphics/P7.C
@@ -1,14 +1,25 @@
 #include <stdio.h>
-// #include <graphics.h>
+#include <graphics.h>
 #include <conio.h>
 #include <math.h>
 
 int main()
 {
-    // int gd = DETECT, gm;
-    // initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
-    float a = sin(100 * 3.14 / 180);
-    printf("%f", a);
-    // closegraph();
+    int gd = DETECT, gm;
+    int x = 0, y = 0;
+    double angle = 0, radians = 0;
+    initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
+
+    line(0, getmaxy() / 2, getmaxx(), getmaxy() / 2);
+
+    for (x = 0; x < getmaxx(); x++)
+    {
+        radians = angle * 3.141 / 180;
+        y = getmaxy() / 2 - 100 * sin(radians);
+        putpixel(x, y, RED);
+        angle += 1;
+    }
+    getch();
+    closegraph();
     return 0;
 }
