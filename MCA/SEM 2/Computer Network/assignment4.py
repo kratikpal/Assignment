2b371cfafd971fb52c60e65e5e760971964d9c55
kratikpal <kratikpal@gmail.com>
2024-05-13 16:34:17 +0530

    network assignment 4

--- MCA/SEM 2/Computer Network/assignment4.py
+++ MCA/SEM 2/Computer Network/assignment4.py
@@ -4,6 +4,21 @@ from scapy.all import IP, TCP, send, sniff
 sender_ip = "10.25.5.164"
 receiver_ip = "10.25.5.168"
 
+# Define sender and receiver IP addresses
+sender_ip = "10.25.5.164"
+receiver_ip = "10.25.5.168"
+# Define destination port numbers
+port1 = 1
+port2 = 5394
+# Define a function to send TCP packets
+def send_tcp_packet(dest_ip, dest_port, flags):
+    packet = IP(dst=dest_ip)/TCP(dport=dest_port, flags=flags)
+    send(packet, verbose=False)
+    send_tcp_packet(receiver_ip, port1, "U")
+    send_tcp_packet(receiver_ip, port2, "")
+    print("Packets sent.")
+
+
 received_packets = sniff(filter=f"tcp and host {receiver_ip}", count=2)
 
 # Print details of received packets
